e42b5e8c5f8f7626fc9bc1d92a510b66895871f3
Priscila Xavier <priscilaxavier672@gmail.com>
2024-07-14 11:25:48 -0300

    Add files via upload

---
+++ HORA-DE-CODAR-JAVINHA/src/MaiorNumero.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MaiorNumero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int primeiroNumero = scanner.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int segundoNumero = scanner.nextInt();
+        System.out.println("Digite o terceiro número: ");
+        int terceiroNumero = scanner.nextInt();
+        System.out.println("Digite o quarto número: ");
+        int quartoNumero = scanner.nextInt();
+
+        if (primeiroNumero > segundoNumero && primeiroNumero > terceiroNumero && primeiroNumero > quartoNumero) {
+            System.out.println("O primeiro número é o maior: " + primeiroNumero);
+        } else if (segundoNumero > primeiroNumero && segundoNumero > terceiroNumero && segundoNumero > quartoNumero) {
+            System.out.println("O segundo número é o maior: " + segundoNumero);
+        } else if (terceiroNumero > primeiroNumero && terceiroNumero > segundoNumero && terceiroNumero > quartoNumero) {
+            System.out.println("O terceiro número é o maior: " + terceiroNumero);
+        } else {
+            System.out.println("O quarto número é o maior: " + quartoNumero);
+        }
+    }
+}
